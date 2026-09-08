16b841d0c1e5bd49500b03210c781b6c88845a67
Abdelrhman fathi <abdoayou4@gmail.com>
2026-09-08 03:14:39 +0300

    Initial commit

---
+++ lib/core/style/color_maingar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/widgets.dart';
+import 'dart:ui';
+
+class ColorMaingar {
+  // --- Brand ---
+  static const Color brandDark = Color(0xff11594B);
+  static const Color brandDefault = Color(0xff1C967E);
+  static const Color brandBackground = Color(0xffF2F9F9);
+  static const Color brandFocused = Color(0xffA9CEC7);
+  static const Color brandPressed = Color(0xff0F4D40);
+  static const Color brandOutline = Color(0xffF2F9F9);
+
+  // --- Neutral ---
+  static const Color neutralPrimary = Color(0xff1D3066);
+  static const Color neutralSecondary = Color(0xff737FA0);
+  static const Color neutralGhost = Color(0xffA9B0C5);
+  static const Color neutralLine = Color(0xffE3E6EC);
+  static const Color neutralBackground = Color(0xffF6F7F9);
+  static const Color neutralWhite = Color(0xffFFFFFF);
+
+  // --- Success ---
+  static const Color successDark = Color(0xff117A7A);
+  static const Color successDefault = Color(0xff17A1A1);
+  static const Color successBackground = Color(0xffE3F3F3);
+  static const Color successFocused = Color(0xffB8D7D7);
+  static const Color successPressed = Color(0xff0F6666);
+  static const Color successOutline = Color(0xff8BD0D0);
+
+  // --- Error ---
+  static const Color errorDark = Color(0xffCC3A55);
+  static const Color errorDefault = Color(0xffFF486A);
+  static const Color errorBackground = Color(0xffFFE9ED);
+  static const Color errorFocused = Color(0xffF0C4CC);
+  static const Color errorPressed = Color(0xffB2324A);
+  static const Color errorOutline = Color(0xffFFA3B4);
+
+  // --- Warning ---
+  static const Color warningDark = Color(0xffE0740F);
+  static const Color warningDefault = Color(0xffFD8311);
+  static const Color warningBackground = Color(0xffFEF0E2);
+  static const Color warningFocused = Color(0xffF6D5B7);
+  static const Color warningPressed = Color(0xffCC6A0E);
+  static const Color warningOutline = Color(0xffFEC188);
+}
